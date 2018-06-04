93fdf99c5f3a2f4fb7becc55540a4873490b36ee
Alexander Ustyuzhanin <alexustmsk@gmail.com>
2018-06-04 23:50:44 +0300

    HelloDate by Thinking in Java ShowProperties

--- src/HelloDate.java
+++ src/HelloDate.java
@@ -1,6 +1,15 @@
-import java.util.Date;
+import java.util.*;
 
+/**Первая программа-пример книги.
+ * Выводит строку и текущее число, а также свойства.
+ * @author Александр Устюжанин
+ * @version 1.0
+ */
 public class HelloDate {
+    /** Точка входа в класс и приложение
+     * @param args Массив строковых аргументов
+     * @throws Exception Исключения не выдаются
+     */
     public static void main(String[] args) {
         System.out.print("Привет, сегодня: ");
         System.out.println(new Date());
